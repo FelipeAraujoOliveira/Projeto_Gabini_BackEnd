f5c7ea9c5549798c2d26b36e3626d5c47d67b7ae
Felipe Araujo de Oliveira <150719778+FelipeAraujoOliveira@users.noreply.github.com>
2024-11-13 19:20:21 -0300

    sonarqube

---
+++ AuthService.cs
@@ -0,0 +1,39 @@
+﻿using Core.Models;
+using Core.Repositories;
+using Core.Services;
+using System.Security.Claims;
+
+namespace Application.Services
+{
+    public class AuthService : IAuthService
+    {
+
+        private readonly IAuthRepository _authRepository;
+        private readonly ITokenService _tokenService;
+
+        public AuthService(IAuthRepository authRepository, ITokenService tokenService)
+        {
+            _authRepository = authRepository;
+            _tokenService = tokenService;
+        }
+
+        public string? GetAuthenticatedUserId(ClaimsPrincipal User)
+        {
+            return User.FindFirst("id")?.Value;
+        }
+
+        public async Task<string> SignIn(string email, string senha)
+        {
+            Usuario? usuario = await _authRepository.GetUsuarioByEmailAndPassword(email, senha);
+            if (usuario == null)
+            {
+                throw new UnauthorizedAccessException("Usuário e/ou senha inválidos");
+            }
+
+            string token = _tokenService.CreateUsuarioToken(usuario);
+
+            return token;
+        }
+
+    }
+}
