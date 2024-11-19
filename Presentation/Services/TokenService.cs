c79917121878a35106ae1bef6e5cd16a5917ca63
FelipeAraujoOliveira <felipearaujooliveirade@gmail.com>
2024-11-19 16:00:51 -0300

    delete migrations, fix method in TokenService

--- Presentation/Services/TokenService.cs
+++ Presentation/Services/TokenService.cs
@@ -9,12 +9,6 @@ namespace Presentation.Services
 {
     public class TokenService : ITokenService
     {
-        private readonly IConfiguration _configuration;
-
-        public TokenService(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
 
         public string CreateUsuarioToken(Usuario usuario)
         {
