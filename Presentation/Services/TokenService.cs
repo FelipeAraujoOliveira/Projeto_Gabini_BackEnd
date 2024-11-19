ab3cd37f2d89050227f0e0a78951ca8d330ac6af
FelipeAraujoOliveira <felipearaujooliveirade@gmail.com>
2024-11-19 15:47:13 -0300

    close jwt key in tokenService

--- Presentation/Services/TokenService.cs
+++ Presentation/Services/TokenService.cs
@@ -19,7 +19,11 @@ namespace Presentation.Services
         public string CreateUsuarioToken(Usuario usuario)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.UTF8.GetBytes(_configuration["Jwt:SECRET_KEY"]);
+            var secretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY") 
+                            ?? throw new InvalidOperationException("JWT_SECRET_KEY não está configurada.");
+
+            var key = Encoding.UTF8.GetBytes(secretKey);
+
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new Claim[]
@@ -27,15 +31,13 @@ namespace Presentation.Services
                     new Claim(ClaimTypes.Name, usuario.NomeCompleto),
                     new Claim(ClaimTypes.Email, usuario.Email),
                     new Claim("id", usuario.Id.ToString()),
-                    // new Claim(ClaimTypes.Role, usuario.Perfil.ToString()),
                 }),
                 Expires = DateTime.UtcNow.AddHours(1),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
 
-            return tokenString;
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
